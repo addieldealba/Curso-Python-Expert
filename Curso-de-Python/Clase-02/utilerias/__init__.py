1b9ae3626c9ba2c57a0466944630dff087c058d3
rctorr <rictor@cuhrt.com>
2018-12-03 18:14:41 -0600

    Se agrega a Clase-02 el módulo archivos al paquete utilerias, además de mejorar la calidad de la documentación tanto del paquete como del módulo.

--- Curso-de-Python/Clase-02/utilerias/__init__.py
+++ Curso-de-Python/Clase-02/utilerias/__init__.py
@@ -1,3 +1,15 @@
 """
-Hola mundo desde los paquetes!
+
+PAQUETE utilerias
+
+    Contiene módulos que resuelven pequeñas tareas
+
+MÓDULOS
+
+archivos
+
+El módulo archivos contiene funciones que permiten realizar operaciones
+con el sistema de archivos del sistema operativo.
+
+
 """
