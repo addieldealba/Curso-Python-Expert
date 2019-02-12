97707a1e4fd9c5f437257c2e5893225f8a7389a5
rctorr <rictor@cuhrt.com>
2019-02-11 22:04:45 -0600

    Se agrega a Clase-03 ejemplo archivos2csv.py haciendo uso de módulos csv, os y nuestro anterior módulo utilerias.archivos con sus respectivas modificaciones para hacer uso del objeto Archivo

---
+++ Curso-de-Python/Clase-03/utilerias/__init__.py
@@ -0,0 +1,15 @@
+"""
+
+PAQUETE utilerias
+
+    Contiene módulos que resuelven pequeñas tareas
+
+MÓDULOS
+
+archivos
+
+El módulo archivos contiene funciones que permiten realizar operaciones
+con el sistema de archivos del sistema operativo.
+
+
+"""
