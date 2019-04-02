bfe3a6a5e6f5737baaa89966c8c08b6b62ac21ef
rctorr <rictor@cuhrt.com>
2019-04-02 03:24:11 -0600

    Se termina Reto 05

--- Curso-de-Python/Clase-05/Reto-05/facturas-crud.py
+++ Curso-de-Python/Clase-05/Reto-05/facturas-crud.py
@@ -2,35 +2,37 @@
 # -*- coding: utf-8 -*-
 
 import click
-import sqlite3
+from modelosqlite3 import *
 
 BD = "facturas.sqlite3"
 
 @click.group()
 def crud():
-    pass
-
+    parametros_bd(BD)
 
 @crud.command()
-def r_cliente():
-    """ Imprime la lista de registros de la tabla Cliente """
-    # Se realiza la conexión a la base de datos y with la cierra en automático
-    with sqlite3.connect(BD) as conn:
-        # Se obtiene un cursor o indice a la base de datos
-        cur = conn.cursor()
-        # Se ejecuta a consulta
-        cur.execute("select * from Cliente")
-        # Se obtiene la lista de campos y se agrega como primer posición en la
-        # lista de resultados.
-        res = [[r[0] for r in cur.description]]
-        # Se obtiene la lista de resultados de la consulta SQL
-        res += cur.fetchall()
+@click.argument("razon-social")
+@click.argument("rfc")
+def c_cliente(razon_social, rfc):
+    """ Inserta un registro en la tabla Cliente """
+    # Variables que definen que insertar y en que tabla se insertan
+    tabla = "Cliente"
+    valores = (razon_social, rfc)
+    # Se realiza la inserción del registro
+    inserta_registro(tabla, valores)
 
+    # Se muestra un mensaje al usuario
+    print("Se ha insertado el registro {} en la tabla {}".format(
+        valores, tabla))
+
+def imprime_texto(registros):
+    """ Imprime la lista de registros en la salida estándar en formato texto """
     # Se obtiene el ancho máximo de cada columna
-    anchos = [[len(str(c)) for c in f] for f in res]
+    anchos = [[len(str(c)) for c in f] for f in registros]
     anchos = [max(f) for f in zip(*anchos)]
+
     # Se imprime la tabla de resultados
-    for fila in res:
+    for fila in registros:
         # Se remplaza los campos vaciós por cademas vacías.
         fila = [c if c != None else "" for c in fila]
         # A cada campo se agrega el valor del ancho
@@ -40,74 +42,39 @@ def r_cliente():
         # Se fusionan los campos en una cadena y se imprimen
         print(" | ".join(fila))
 
+@crud.command()
+def r_cliente():
+    """ Imprime la lista de registros de la tabla Cliente """
+    # Se obtiene la lista de registros de la tabla Autor
+    registros = obtiene_registros("Cliente")
+    # Se imprimen los registros en formato texto en la salida estándar
+    imprime_texto(registros)
 
 @crud.command()
-@click.argument("razon-social")
-@click.argument("rfc")
-def c_cliente(razon_social, rfc):
-    """ Inserta un registro en la tabla Cliente """
-    # Se realiza la conexión a la base de datos y se mantiene abierta hasta
-    # terminar
-    with sqlite3.connect(BD) as conn:
-        # Se obtiene un cursor o indice a la base de datos
-        cur = conn.cursor()
-        # Se arma la consulta SQL
-        sql = "insert into Cliente values (null, ?, ?)"
-        # Se arma una tupla de valores con los campos
-        valores = (razon_social, rfc)
-        # Se ejecuta la consulta
-        cur.execute(sql, valores)
-        # Se ejecuta un commit para indicar que la inserción se ejecute como una
-        # operación atómica.
-        conn.commit()
-
-    # Se muestra un mensaje al usuario
-    print("Se ha insertado el registro {} en la tabla Cliente".format(valores))
-
-@crud.command()
-@click.argument("idcliente")
+@click.argument("id")
 @click.argument("campo")
 @click.argument("valor")
-def u_cliente(idcliente, campo, valor):
+def u_cliente(id, campo, valor):
     """ Actualiza los datos de un Cliente """
-    # Se realiza la conexión a la base de datos
-    with sqlite3.connect(BD) as conn:
-        # Se obtiene un cursor o indice a la base de datos
-        cur = conn.cursor()
-        # Se crea la consulta SQL
-        sql = "update Cliente set {}=? where idCliente=?".format(campo)
-        # Se crea la tupla de valores
-        valores = (valor, idcliente)
-        # Se ejecuta la consulta SQL agregando los valores de forma segura
-        cur.execute(sql, valores)
-        # Se ejecuta un commit para indicar a la BD que la actualización se
-        # ejecute como una operación atómica.
-        conn.commit()
+    # Variables necesarias para actualizar un campo en un registro
+    tabla = "Cliente"
+    valores = (valor, id)
+    actualiza_registro(tabla, campo, valores)
 
     # Se muestra un mensaje al usuario
-    print("Se ha actualizado el registro {} en la tabla Cliente".format(idcliente))
-
+    print("Se ha actualizado el registro {} en la tabla {}".format(id, tabla))
 
 @crud.command()
-@click.argument("idcliente")
-def d_cliente(idcliente):
+@click.argument("id")
+def d_cliente(id):
     """ Elimina un registro de un Cliente """
-    # Se realiza la conexión a la base de datos
-    with sqlite3.connect(BD) as conn:
-        # Se obtiene un cursor o indice a la base de datos
-        cur = conn.cursor()
-        # Se crea la consulta SQL
-        sql = "delete from Cliente where idCliente=?"
-        # Se crea la tupla de valores
-        valores = (idcliente,)
-        # Se ejecuta la consulta SQL agregando los valores de forma segura
-        cur.execute(sql, valores)
-        # Se ejecuta un commit para indicar a la BD que la aliminación se
-        # ejecute como una operación atómica.
-        conn.commit()
+    # Variables necesarias para eliminar un campo en un registro
+    tabla = "Cliente"
+    valores = (id,)
+    elimina_registro(tabla, valores)
 
     # Se muestra un mensaje al usuario
-    print("Se ha eliminado el registro {} en la tabla Cliente".format(idcliente))
+    print("Se ha eliminado el registro {} en la tabla {}".format(id, tabla))
 
 
 if __name__ == '__main__':
